fdb62397e73c808ee4e8dc8fdc951a855f39cb7e
Mani Sai Prasoon <prasoon@Prasoons-MacBook-Pro.local>
2023-12-26 14:04:54 +0530

    Added basename

--- src/App.js
+++ src/App.js
@@ -1,37 +1,36 @@
-import './App.css';
-import { Box } from '@mui/material';
-import { useEffect } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import SubmitTest from './components/Conclusions/SubmitTest';
-import ExamContainer from './components/ExamForms/ExamContainer';
-import StudentDetailsForm from './components/ExamForms/StudentDetailsForm';
-import Robot from './components/Robot/robot';
+import "./App.css";
+import { Box } from "@mui/material";
+import { useEffect } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SubmitTest from "./components/Conclusions/SubmitTest";
+import ExamContainer from "./components/ExamForms/ExamContainer";
+import StudentDetailsForm from "./components/ExamForms/StudentDetailsForm";
+import Robot from "./components/Robot/robot";
 
 function App() {
-
   useEffect(() => {
     // handle.enter();
   }, []);
   return (
-    <div className='main-container'>
-        <BrowserRouter>
-          <Box
-            sx={{
-              display: 'flex',
-              justifyContent: 'flex-start',
-              width: '100%',
-              padding: '1%',
-              backgroundColor: '#253644',
-              top: 0,
-            }}
-          ></Box>
-          <Routes>
-            <Route path='/' element={<Robot />} />
-            <Route path='/form' element={<StudentDetailsForm />} />
-            <Route path='/form/exam' element={<ExamContainer />} />
-            <Route path='/submit-test' element={<SubmitTest />} />
-          </Routes>
-        </BrowserRouter>
+    <div className="main-container">
+      <BrowserRouter basename={window.location.pathname || ""}>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "flex-start",
+            width: "100%",
+            padding: "1%",
+            backgroundColor: "#253644",
+            top: 0,
+          }}
+        ></Box>
+        <Routes>
+          <Route path="/" element={<Robot />} />
+          <Route path="/form" element={<StudentDetailsForm />} />
+          <Route path="/form/exam" element={<ExamContainer />} />
+          <Route path="/submit-test" element={<SubmitTest />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
